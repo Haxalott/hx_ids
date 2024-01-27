bfd6443463eb8b1dd2a77d2a3093fa01e5c6eeca
Haxalott <43390732+jcwaz@users.noreply.github.com>
2024-01-27 14:33:30 +1100

    Added files.

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Haxalott'
+version '1.0.0'
+
+server_script 'server/main.lua'
+shared_script '@ox_lib/init.lua'
---
+++ server/main.lua
@@ -0,0 +1,10 @@
+lib.addCommand('id', {
+    help = 'Displays your ID'
+}, function(source, args, raw)
+    TriggerClientEvent('ox_lib:notify', source, {
+        title = 'City',
+        description = 'Your current ID is '.. source,
+        type = 'inform'
+    })
+
+end)
