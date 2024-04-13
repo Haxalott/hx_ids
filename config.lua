e327d7cab2026de4f7ac8a273a32404bce01dfaf
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-04-13 23:43:31 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,4 +5,3 @@ Config.Duration = '500' -- Duration
 Config.Icon = 'city' -- To change look at: https://fontawesome.com/icons
 Config.IconColor = 'white' -- Color of icon
 Config.NotifyPosition = 'top-center' -- 'top' or 'top-right' or 'top-left' or 'bottom' or 'bottom-right' or 'bottom-left' or 'center-right' or 'center-left'
-a
