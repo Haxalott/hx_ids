6fd16c41ce037596f5bf4deb42554d4a49f6129e
Haxalott's Developments <haxalott@proton.me>
2025-04-21 22:04:32 +1000

    Fix(notify): Added missing source param
    Oops...

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import Config from '@common/config';
 import { addCommand } from '@overextended/ox_lib/server';
 
 addCommand('id', async (source: number) => {
-    emitNet('ox_lib:notify', {
+    emitNet('ox_lib:notify', source, {
         title: Config.Title,
         description: `Your ID is: ${source}`,
         position: Config.Position
